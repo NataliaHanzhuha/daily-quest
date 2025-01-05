86737daa60a7410848647d7e6a72725d1163043f
NHanzhuha <NHanzhuha@healthprecision.com>
2025-01-05 11:29:44 -0700

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,13 @@ import { Component } from '@angular/core';
     standalone: false
 })
 export class AppComponent {
-  title = 'daily-quest';
+  firebaseConfig: any = {
+    apiKey: "AIzaSyBWXJPkOBtpHfdBI55ceUu_jPdqSSoXe2M",
+    authDomain: "daily-quest-3361f.firebaseapp.com",
+    projectId: "daily-quest-3361f",
+    storageBucket: "daily-quest-3361f.firebasestorage.app",
+    messagingSenderId: "566987286470",
+    appId: "1:566987286470:web:3075c3dd7b9569718974c7",
+    measurementId: "G-PS5161CLDQ"
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+// import { AngularFireModule } from '@angular/fire';
+// import { AngularFireAuthModule } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // AngularFireModule.initializeApp(environment.firebase),
+    // AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
